d2cdcdf0aa98c4a680ff261666aabcb9e5f29e4a
faizrafii <m.faiz8@yahoo.com>
2022-04-03 22:52:28 +0700

    edit process env password

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const password = process.env.PASSWORD;
-const testpass = '123';
-const connAtlas = 'mongodb://faiz1:' + testpass + '@cluster0-shard-00-00.bd5hs.mongodb.net:27017,cluster0-shard-00-01.' +
+const connAtlas = 'mongodb://faiz1:' + password + '@cluster0-shard-00-00.bd5hs.mongodb.net:27017,cluster0-shard-00-01.' +
 'bd5hs.mongodb.net:27017,cluster0-shard-00-02.bd5hs.mongodb.net:27017/cluster0?ssl=true&' +
 'replicaSet=atlas-10erh7-shard-0&authSource=admin&retryWrites=true&w=majority';
 console.log(connAtlas);
-// const srvConn =
 
 const connLocal = 'mongodb://mainuser:123@localhost:27017/mongoosedb?authSource=admin';
 
